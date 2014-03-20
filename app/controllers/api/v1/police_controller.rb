0dfab5073f70b91fb13cb2a49b2cd457a4a890e3
deedoubleyou <deedoubleyou@gmail.com>
2014-03-20 18:45:49 +0000

    Added traffic data

--- app/controllers/api/v1/police_controller.rb
+++ app/controllers/api/v1/police_controller.rb
@@ -8,8 +8,6 @@ class Api::V1::PoliceController < ApplicationController
   end
 
   def crimes
-
-
     crime_json = File.read("storage/crime.json")
     @crimes = JSON.parse(crime_json)
     @crimes.sort_by! { |c| c['Year'] }
@@ -18,6 +16,18 @@ class Api::V1::PoliceController < ApplicationController
       format.json { render json: @crimes }
       format.xml { render xml: @crimes }
       format.html { render html: @crimes }
+    end
+  end
+
+  def traffic
+    traffic_json = File.read("storage/traffic.json")
+    @traffic = JSON.parse(traffic_json)
+    @traffic.sort_by! { |c| c['Year'] }
+
+    respond_to do |format|
+      format.json { render json: @traffic }
+      format.xml { render xml: @traffic }
+      format.html { render html: @traffic }
     end
   end
 end
