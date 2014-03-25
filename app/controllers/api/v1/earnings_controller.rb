da8726c8ade76dad20a7b1217d72c66ba9ada1a4
deedoubleyou <deedoubleyou@gmail.com>
2014-03-25 18:16:13 +0000

    Set front page to 4 cols and added earnings charts

--- app/controllers/api/v1/earnings_controller.rb
+++ app/controllers/api/v1/earnings_controller.rb
@@ -1,6 +1,9 @@
 require 'json'
 
 class Api::V1::EarningsController < ApplicationController
+  before_action :set_earnings_by_sex, only: [:earnings_sex, :index]
+  before_action :set_earnings_by_age_group, only: [:earnings_age_group, :index]
+
   def index
     respond_to do |format|
       format.html { render :index }
@@ -8,14 +11,12 @@ class Api::V1::EarningsController < ApplicationController
   end
 
   def earnings_age_group
-    earnings_json = File.read("storage/earnings_age_group.json")
-    @earnings = JSON.parse(earnings_json)
-    @earnings.sort_by! { |c| c['Year'] }
+
 
     respond_to do |format|
-      format.json { render json: @earnings }
-      format.xml { render xml: @earnings }
-      format.html { render html: @earnings }
+      format.json { render json: @earnings_age_group }
+      format.xml { render xml: @earnings_age_group }
+      format.html { render html: @earnings_age_group }
     end
   end
 
@@ -32,14 +33,26 @@ class Api::V1::EarningsController < ApplicationController
   end
 
   def earnings_sex
-    earnings_json = File.read("storage/earnings_sex.json")
-    @earnings = JSON.parse(earnings_json)
-    @earnings.sort_by! { |c| c['Year'] }
+
 
     respond_to do |format|
-      format.json { render json: @earnings }
-      format.xml { render xml: @earnings }
-      format.html { render html: @earnings }
+      format.json { render json: @earnings_sex }
+      format.xml { render xml: @earnings_sex }
+      format.html { render html: @earnings_sex }
     end
+  end
+
+  private
+
+  def set_earnings_by_sex
+    earnings_json = File.read("storage/earnings_sex.json")
+    @earnings_sex = JSON.parse(earnings_json)
+    @earnings_sex.sort_by! { |c| c['Year'] }
+  end
+
+  def set_earnings_by_age_group
+    earnings_json = File.read("storage/earnings_age_group.json")
+    @earnings_age_group = JSON.parse(earnings_json)
+    @earnings_age_group.sort_by! { |c| c['Year'] }
   end
 end
