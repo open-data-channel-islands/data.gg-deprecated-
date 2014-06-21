ffb584770f0a00f3082bf2e0dbfcc3726da9ca58
Kieran Senior <kieran@kieransenior.co.uk>
2014-06-21 21:25:02 +0100

    Fix for not showing correct times when they're between midnight and 1am. Made a y-axis scroll overflow so the page looks and behaves like normal when there are many items on screen.

--- app/models/stop_link.rb
+++ app/models/stop_link.rb
@@ -8,6 +8,16 @@ class StopLink < ActiveRecord::Base
   
   def time_string
     time_s = time.to_s
+    
+    if time >= 0
+      if time < 10
+        time_s = "000" + time_s
+      elsif time < 100
+        time_s = "00" + time_s
+      end
+    end
+    
+    
     minute = time_s[time_s.length - 2, time_s.length - 1]
     if time_s.length == 3
       hour = time_s[0, 1]
