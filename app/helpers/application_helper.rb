295bda92c55fac6b9828b1b9be834c6f921dc67b
Jason Magee <deedoubleyou@gmail.com>
2015-09-04 18:10:45 +0100

    Refs #33 Added way to have strings for each location. Add them to /gg/strings.yml and /je/strings.yml and use them like so: storage_str('key')

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def action?(controller, action)
     controller == params[:controller] && params[:action] == action
   end
+
+  def storage_str(name)
+    YAML.load(File.read("storage/#{ENV['place_code']}/strings.yml"))[name]
+  end
 end
