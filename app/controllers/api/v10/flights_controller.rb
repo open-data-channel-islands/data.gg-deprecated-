908c833751936c908b4bb84d748e48ff00de4d3a
Jason Magee <deedoubleyou@gmail.com>
2015-09-08 18:52:08 +0100

    Added memory caching for arrivals and departures for 30 seconsd

--- app/controllers/api/v10/flights_controller.rb
+++ app/controllers/api/v10/flights_controller.rb
@@ -6,7 +6,14 @@ class Api::V10::FlightsController < ApplicationController
 
   def departures
     @title = 'Departures'
-    @flights = FlightsParser::get_departures
+
+    if Rails.cache.exist? 'departures'
+      @flights = Rails.cache.fetch('departures')
+    else
+      @flights = FlightsParser::get_departures
+      Rails.cache.write('departures', @flights, expires_in: 30.seconds)
+    end
+
     respond_to do |format|
       format.json { render json: @flights }
       format.xml { render xml: @flights }
@@ -16,7 +23,15 @@ class Api::V10::FlightsController < ApplicationController
 
   def arrivals
     @title = 'Arrivals'
-    @flights = FlightsParser::get_arrivals
+
+    if Rails.cache.exist? 'arrivals'
+      @flights = Rails.cache.fetch('arrivals')
+    else
+      @flights = FlightsParser::get_arrivals
+      Rails.cache.write('arrivals', @flights, expires_in: 30.seconds)
+    end
+
+
     respond_to do |format|
       format.json { render json: @flights }
       format.xml { render xml: @flights }
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,11 +23,13 @@ DataGg::Application.configure do
 
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load
-  
+
   #config.assets.precompile += %w( *.scss bootstrap.css bootstrap.min.css bootstrap.js bootstrap.min.js Chart.js Chart.min.js )
   config.assets.precompile += %w( *.css *.js )
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.cache_store = :memory_store # This is usually disabled for dev but it's important for the flights so I've left it on here
 end
