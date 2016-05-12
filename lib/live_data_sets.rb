9fd6fdc03114631f5dbe0811f4bdb1b4cb748a15
Jason Magee <contact@jasemagee.com>
2016-05-12 19:12:08 +0100

    Fixed bug found by Adrian with caching returning bool instead of a data array

--- lib/live_data_sets.rb
+++ lib/live_data_sets.rb
@@ -1,21 +1,25 @@
 class LiveDataSets
 
   def self.get_flights_arrivals
+    flights = []
     if Rails.cache.exist? 'arrivals'
-      @flights = Rails.cache.fetch('arrivals')
+      flights = Rails.cache.fetch('arrivals')
     else
-      @flights = FlightsParser::get_arrivals
-      Rails.cache.write('arrivals', @flights, expires_in: 30.seconds)
+      flights = FlightsParser::get_arrivals
+      Rails.cache.write('arrivals', flights, expires_in: 30.seconds)
     end
+    flights
   end
 
   def self.get_flights_departures
+    flights = []
     if Rails.cache.exist? 'departures'
-      @flights = Rails.cache.fetch('departures')
+      flights = Rails.cache.fetch('departures')
     else
-      @flights = FlightsParser::get_departures
-      Rails.cache.write('departures', @flights, expires_in: 30.seconds)
+      flights = FlightsParser::get_departures
+      Rails.cache.write('departures', flights, expires_in: 30.seconds)
     end
+    flights
   end
 
   def self.get_harbour_sailings
