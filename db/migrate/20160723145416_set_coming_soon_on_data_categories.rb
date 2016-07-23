587725ad4bf850dfaee10e203ae80d8f4dff46f4
Jason Magee <contact@jasemagee.com>
2016-07-23 16:32:08 +0100

    Need to update column info for migration data changes to work

--- db/migrate/20160723145416_set_coming_soon_on_data_categories.rb
+++ db/migrate/20160723145416_set_coming_soon_on_data_categories.rb
@@ -1,26 +1,19 @@
 class SetComingSoonOnDataCategories < ActiveRecord::Migration[5.0]
   def change
-
+    DataCategory.reset_column_information
     tides = DataCategory.find_by(name: 'Tides')
-    tides.coming_soon = true
-    tides.save!
+    tides.update!(coming_soon: true)
     fuel = DataCategory.find_by(name: 'Fuel')
-    fuel.coming_soon = true
-    fuel.save!
+    fuel.update!(coming_soon: true)
     sports = DataCategory.find_by(name: 'Sports')
-    sports.coming_soon = true
-    sports.save!
+    sports.update!(coming_soon: true)
     broadband = DataCategory.find_by(name: 'Broadband')
-    broadband.coming_soon = true
-    broadband.save!
+    broadband.update!(coming_soon: true)
     recycling = DataCategory.find_by(name: 'Recycling')
-    recycling.coming_soon = true
-    recycling.save!
+    recycling.update!(coming_soon: true)
     roadworks = DataCategory.find_by(name: 'Roadworks')
-    roadworks.coming_soon = true
-    roadworks.save!
+    roadworks.update!(coming_soon: true)
     fishing = DataCategory.find_by(name: 'Fishing')
-    fishing.coming_soon = true
-    fishing.save!
+    fishing.update!(coming_soon: true)
   end
 end
--- db/migrate/20160723145704_set_show_on_website_on_data_categories.rb
+++ db/migrate/20160723145704_set_show_on_website_on_data_categories.rb
@@ -1,8 +1,7 @@
 class SetShowOnWebsiteOnDataCategories < ActiveRecord::Migration[5.0]
   def change
-
+    DataCategory.reset_column_information
     gov_spending = DataCategory.find_by(name: 'Government spending')
-    gov_spending.show_on_website = false
-    gov_spending.save!
+    gov_spending.update!(show_on_website: false)
   end
 end
