fc2dfa29b47fba350b8f50fc3146410e557e1472
Jason Magee <contact@jasemagee.com>
2017-03-27 17:48:11 +0100

    Fixed tides controller causing test failure. All tests pass

--- app/controllers/api/v10/tides_controller.rb
+++ app/controllers/api/v10/tides_controller.rb
@@ -1,4 +1,14 @@
 class Api::V10::TidesController < ApplicationController
   def tides
+    @title = 'Tides'
+    tides_json = File.read("storage/#{ENV['place_code']}/tides/tides.json")
+    @tides = JSON.parse(tides_json)
+
+    respond_to do |format|
+      format.json { render json: @tides }
+      format.xml { render xml: @tides }
+      format.html { render :tides, layout: ((params[:layout].nil? || params[:layout] == 'true') ? true : false) }
+    end
   end
+
 end
