2d6169b690e37098279e8731d8421fd52322dd74
Jason Magee <contact@jasemagee.com>
2017-03-27 15:48:35 +0100

    Unable to match Bens 2015 numbers for earnings by sector. Found on gov.gg and updated. 2016 fine

--- db/migrate/20170327142837_updates_for_bens_changes.rb
+++ db/migrate/20170327142837_updates_for_bens_changes.rb
@@ -1,7 +1,7 @@
 class UpdatesForBensChanges < ActiveRecord::Migration[5.0]
   def change
 
-    prison_pop = DataCategory.find_by(name: 'Prison Population')
+    prison_pop = DataSet.find_by(name: 'Prison Population')
     prison_pop.update!(source_url: 'https://www.gov.gg/prison')
 
 
