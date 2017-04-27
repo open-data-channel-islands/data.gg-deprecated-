17da4dea53b0fb3001b64f17ecd9913f6261d2c4
Jason Magee <contact@jasemagee.com>
2017-04-27 10:49:41 +0100

    Updated buses to March 2017

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,50 +7,7 @@ class HomeController < ApplicationController
   before_action :set_data_categories, only: [:index, :developers, :charts]
 
   def index
-    colors = [
-      ["rgba(255, 240, 110, 0.5)", "rgba(193, 186, 123, 0.5)"],
-    ["rgba(187, 255, 147, 0.5)", "rgba(87, 149, 50, 0.5)"],
-    ["rgba(147, 255, 220, 0.5)", "rgba(38, 113, 88, 0.5)"],
-    ["rgba(255, 183, 147, 0.5)", "rgba(170, 95, 57, 0.5)"],
-    ["rgba(255, 147, 172, 0.5)", "rgba(156, 52, 76, 0.5)"],
-    ["rgba(147, 187, 255, 0.5)", "rgba(38, 87, 149, 0.5)"]
-    ]
-
-    bus_usage_json = File.read("storage/#{ENV['place_code']}/buses/bus_usage.json")
-    bus_usage = JSON.parse(bus_usage_json)
-
-    @years = bus_usage.uniq { |p| p['Year'] }.collect { |p| p['Year'] }
-
-    # Months for looping over data
-    month_itter = ['January','February','March','April','May',
-      'June','July','August','September','October','November','December']
-
-    # Create a hash so you have bus_usage[year] containing an array of key-value pairs for month->value
-    bus_usage = Hash[*bus_usage.map { |k| [k['Year'], k] }.flatten]
-
-    @bus_data = []
-
-    @years.each do |year|
-      values = []
-      curr_usage = bus_usage[year]
-
-      month_itter.each do |month|
-        val = curr_usage[month]
-        # Only show it if it exists
-        if val != nil
-          values << val
-        else
-          values << 0
-        end
-      end
-
-      color = colors[colors.length - 1]
-      colors.delete_at(colors.length - 1)
-      @bus_data << { name: year, data: values, color: color[0], borderColor: color[1] }
-    end
-
     @active_alerts = Alert.where(active: true).order(created_at: :desc)
-
     respond_to do |format|
       format.json
       format.xml
