94ebe39d1f9eda568e45cb16549cdf4970a2ed31
Jason Magee <deedoubleyou@gmail.com>
2018-01-12 23:10:11 +0000

    please deploy...

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,6 +36,8 @@ set :linked_files, %w{config/database.yml config/secrets.yml config/application.
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"") # Read from local system
+
 namespace :deploy do
 
   desc 'Restart application'
