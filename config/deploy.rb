d9330240fc414755ba58b309b6abf2633b134613
Jason Magee <deedoubleyou@gmail.com>
2018-01-13 19:45:17 +0000

    Updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,8 @@ set :application, 'data.gg'
 set :repo_url, 'git@github.com:open-data-channel-islands/data.gg.git'
 set :deploy_to, '/srv/data'
 
-set :linked_files, %w{config/database.yml config/secrets.yml config/application.yml public/sitemap.xml}
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 
+'config/application.yml', 'public/sitemap.xml', 'config/puma.rb')
 
 set :rvm_type, :user                     # Defaults to: :auto
 set :rvm_ruby_version, '2.4.1-p0'      # Defaults to: 'default'
