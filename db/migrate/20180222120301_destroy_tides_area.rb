aa63cae51d314a498fed4b88a2b7d7e9be26f26d
Jason Magee <deedoubleyou@gmail.com>
2018-02-22 12:07:44 +0000

    Oops, wrong way round

--- db/migrate/20180222120301_destroy_tides_area.rb
+++ db/migrate/20180222120301_destroy_tides_area.rb
@@ -2,14 +2,16 @@ class DestroyTidesArea < ActiveRecord::Migration[5.1]
   def change
     gg = Place.find_by(code: 'GG')
 
-    tides_category = DataCategory.find_by(name: 'Tides')
-    if !tides_category.nil?
-      tides_category.destroy
-    end
 
     tides_set = DataSet.find_by(name: 'Tides')
     if !tides_set.nil?
       tides_set.destroy
     end
+    
+    tides_category = DataCategory.find_by(name: 'Tides')
+    if !tides_category.nil?
+      tides_category.destroy
+    end
+
   end
 end
