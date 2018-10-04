1548fd581b61d16c639a39f8140a40a07cd24aec
Jason Magee <deedoubleyou@gmail.com>
2018-10-04 20:06:12 +0100

    Updates for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for Capistrano 3.1
-lock '3.10.1'
+lock '3.11.0'
 
 set :application, 'data.gg'
 set :repo_url, 'git@github.com:open-data-channel-islands/data.gg.git'
@@ -9,4 +9,4 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 'config/application.yml', 'public/sitemap.xml', 'config/puma.rb')
 
 set :rvm_type, :user                     # Defaults to: :auto
-set :rvm_ruby_version, '2.4.1-p0'      # Defaults to: 'default'
+set :rvm_ruby_version, '2.5.1-p0'      # Defaults to: 'default'
